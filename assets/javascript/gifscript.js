548c218c0e785c90815f322d9dcce9fe52f5f24a
origami unicorn <rvwright@gmail.com>
2019-11-15 16:22:02 -0800

    Added to the on click for add-gif so that the input field is cleared on each click.

--- assets/javascript/gifscript.js
+++ assets/javascript/gifscript.js
@@ -1,13 +1,13 @@
-var gifs = ["Aardvark", "Elephant", "Rainstorm"];
+var topics = ["Aardvark", "Elephant", "Rainstorm"];
 
 function renderButtons() {
     $("#buttonDiv").empty();
 
-    for (var i = 0; i < gifs.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
-        a.addClass("gif");
-        a.attr("data-name", gifs[i]);
-        a.text(gifs[i]);
+        a.addClass("gifTopic");
+        a.attr("data-topic", topics[i]);
+        a.text(topics[i]);
         $("#buttonDiv").append(a);
     }
 }
@@ -18,13 +18,43 @@ $("#add-gif").on("click", function () {
 
     var gifSearchTerm = $("#gif-input").val().trim();
 
-    if (!gifs.includes(gifSearchTerm)) {
+    if (!topics.includes(gifSearchTerm)) {
         $("#buttonDiv").append("<button>" + gifSearchTerm + "</button>");
-        gifs.push(gifSearchTerm);
+        topics.push(gifSearchTerm);
+        $("#gif-input").val("");
     };
 
     renderButtons();
 
+});
+
+$("button").on("click", function () {
+    var topic = $(this).attr("data-topic");
+    console.log(topic);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topic + "&api_key=Sk691sxiwZCXghJC3KzxBALR0DPLYXLm&limit=10";
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(gifImage);
+
+                $("#retrievedGifs").prepend(gifDiv);
+            }
+        });
 });
 
 // Calling the renderButtons function to display the initial gif search terms
